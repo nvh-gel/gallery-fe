c4cc482b0d96ab42de07dc4067b39c22386152c3
nvh-gel <nvhien2703@outlook.com>
2020-07-18 17:08:21 +0700

    first commit

---
+++ src/AppNavBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavLink, NavItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+
+class AppNavBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: false };
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+        return (
+            <Navbar color="dark" dark expand="md">
+                <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="https://www.google.com">Google</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://www.github.com">GitHub</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default AppNavBar;
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import AppNavBar from './AppNavBar'
+import { Container, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <AppNavBar />
+                <Container fluid>
+                    <Button color="link">
+                        <Link to="/groups">Manage JUG Tours</Link>
+                    </Button>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Home;
